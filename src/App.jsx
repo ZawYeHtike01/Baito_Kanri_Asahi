067aac21fa14509fc8abadb729d6a45c22d29251
Zaw <zawyaehtike9.adv@gmail.com>
2025-12-03 14:06:21 +0900

    add list style

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,8 @@ import Template from './Template';
 import ProtectedRoute from './ProtoctedRouted';
 import Home from './Pages/Home';
 import WorkList from './Pages/WorkList';
-
+import GetJapaneseHolidays from './Pages/Data';
+import { useEffect } from 'react';
 
 const AppContext=createContext();
 
@@ -51,8 +52,17 @@ function App() {
   const [showDrawer,setShowDrawer]=useState(false);
   const [globalMsg, setGlobalMsg ]=useState(null);
   const [selectedDate, setSelectedDate] = useState(null);
+  
+  const [JapanseHolidays,setJapaneseHolidays]=useState([]);
+  useEffect(() => {
+      async function fetchHolidays() {
+        const holidays = await GetJapaneseHolidays();
+        setJapaneseHolidays(holidays);
+      }
+      fetchHolidays();
+  }, []);
   return (
-    <AppContext.Provider value={{auth,setAuth,showDrawer,setShowDrawer,globalMsg, setGlobalMsg,selectedDate, setSelectedDate}}>
+    <AppContext.Provider value={{JapanseHolidays,setJapaneseHolidays,auth,setAuth,showDrawer,setShowDrawer,globalMsg, setGlobalMsg,selectedDate, setSelectedDate}}>
      
 		<RouterProvider router={router} />
 	  
